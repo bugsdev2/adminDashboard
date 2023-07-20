6e53b0ac63b417b197b2cf25e0e344dcf8baf2c3
BugsDev2 <msherlock246@gmail.com>
2023-07-20 10:21:45 +0530

    Made appropriate changes to main stylesheets, main js file and index.html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import '../style/style.css'
+import '../style/style.css';
+import '../style/header.css';
+import '../style/dashboard.css';
+import '../style/utils.css';
+
+import buttonResize from '../src/button.js';
+import dashResize from '../src/dashboard.js';
+
+buttonResize();
+dashResize();
